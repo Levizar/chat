8639cac234392c88ef9329c003576405179f874f
Brice Levizar Bartoletti <bartolettibrice@gmail.com>
2020-01-06 17:03:14 +0100

    comment code

--- server.js
+++ server.js
@@ -1,26 +1,39 @@
+// import mod fs (API to interact with file system) and crypto (mod to crypt the password)
 const fs = require("fs");
 const crypto = require("crypto");
 
 // ------------------ MYSQL ------------------ \\
 
+// crypt ID for token
 const id = crypto.randomBytes(16).toString("hex");
 console.log(id);
 
+// import mysql
 const mysql = require("mysql");
-const {user, password} = JSON.parse(fs.readFileSync("login.json"));
+
+// need to see an exemple of login.json
+// Why json if sql?
+const {
+    user,
+    password
+} = JSON.parse(fs.readFileSync("login.json"));
+
+// Connection to the data base
 const db = mysql.createConnection({
-    host     : "localhost",
-    user     : user,
-    password : password,
-    port     : 3300,
-    database : "chat"
+    host: "localhost",
+    user: user,
+    password: password,
+    port: 3300,
+    database: "chat"
 });
 
 db.connect();
 
 // ----------------- EXPRESS ----------------- \\
 
+// import module express
 const express = require("express");
+// initialize the express server
 const app = express();
 const http = require("http").createServer(app);
 
@@ -32,15 +45,19 @@ app.use(express.static(__dirname + "/public"));
 // answer to 404 status
 app.get("*", (_, res) => res.status(404).send("error 404"));
 
-//// Louis' method for post request (= node vanilla method) : see if
+//// Louis' method for post request (= node vanilla method) : see if it exists in express
 // here is how we handle the post request
 app.post("/html/ptdr", (req, res) => {
     let data = "";
     // Data are received piece by piece and not entirely.
+    // Thus we have to reconstruct the data received.
     req.on("data", chunk => {
         data += chunk;
+        // if the data is a flood attack : kill the request
         if (data.length > 1e3) req.destroy();
     });
+    // at the end of the request : execute this function
+    // For example: here we 
     req.on("end", () => {
         const signUp = JSON.parse(data);
         db.query(`SELECT * FROM users WHERE username = '${signUp.username}' LIMIT 1`, (err, rows) => {
@@ -63,11 +80,12 @@ http.listen(3000, () => {
 //     if (err) throw err;
 //     console.log(rows);
 // });
-  
+
 // db.end();
 
 // ---------------- SOCKET.IO ---------------- \\
 
+// import module socket.io
 const io = require("socket.io")(http);
 
 io.on("connection", socket => {
@@ -87,8 +105,8 @@ io.on("connection", socket => {
             content: message,
             author: socket.id
         };
-        
+
         socket.emit("message", newMessage)
-              .broadcast.emit("message", newMessage);
+            .broadcast.emit("message", newMessage);
     })
 });
