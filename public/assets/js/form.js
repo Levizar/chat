9339d99385935c1903472db43ebac78fda889202
maelys26 <35763602+maelys26@users.noreply.github.com>
2020-01-08 16:40:50 +0100

    preventDefault + regex on username

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -1,8 +1,31 @@
+//Display or not the forms
 document.getElementById('signUpLink').addEventListener("click", () => {
     document.getElementById('signUpForm').style.display= 'block';
     document.getElementById('signInForm').style.display="none";
-})
+});
+
+document.getElementById('signInLink').addEventListener("click", () => {
+    document.getElementById('signUpForm').style.display= 'none';
+    document.getElementById('signInForm').style.display="block";
+});
 
 
-//regex.test(variable)
-///\w/g
+//Don't allow to send the forms
+const formSignUp = document.getElementById('signUp');
+const formSignIn = document.getElementById('signIn');
+
+formSignUp.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Fail to send the form')
+    const regex = /\W/g;
+    let usernameSignIn = document.getElementById('usernameConnection').value;
+    console.log(!regex.test(usernameSignIn)); 
+});
+
+formSignIn.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log('Fail to send the form')
+    const regex = /\W/g;
+    let usernameSignIn = document.getElementById('usernameConnection').value;
+    console.log(!regex.test(usernameSignIn)); 
+});
