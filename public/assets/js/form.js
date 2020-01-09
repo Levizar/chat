1ed27160262e5c4c5fff7666490f4b109f719a6d
maelys26 <35763602+maelys26@users.noreply.github.com>
2020-01-09 14:24:17 +0100

    add script for sign

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -12,20 +12,39 @@ document.getElementById('signInLink').addEventListener("click", () => {
 
 //Don't allow to send the forms
 const formSignUp = document.getElementById('signUp');
-const formSignIn = document.getElementById('signIn');
+const formSignIn = document.getElementById('signInBtn');
 
 formSignUp.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Fail to send the form')
     const regex = /\W/g;
-    let usernameSignIn = document.getElementById('usernameConnection').value;
-    console.log(!regex.test(usernameSignIn)); 
+    if(!regex.test(usernameSignUp)){
+        let usernameSignUp = document.getElementById('usernameSignUp').value;
+        let passwordSignUp = document.getElementById('pwdSignUp').value;
+        let mailSignUp = document.getElementById('mailSignUp').value;
+        let data = {username : usernameSignUp,
+                    password: passwordSignUp,
+                    mail : mailSignUp};
+        data = JSON.stringify(data);
+        fetch("/signup", {
+            method: "POST",
+            body: data
+        });
+    }; 
 });
 
 formSignIn.addEventListener('submit', (event) => {
     event.preventDefault();
-    console.log('Fail to send the form')
     const regex = /\W/g;
-    let usernameSignIn = document.getElementById('usernameConnection').value;
-    console.log(!regex.test(usernameSignIn)); 
+    if(!regex.test(usernameSignIn)){
+        let usernameSignIn = document.getElementById('usernameSignIn').value;
+        let passwordSignIn = document.getElementById('pwdSignUp').value;
+        let data = {username : usernameSignIn,
+                    password: passwordSignIn};
+        data = JSON.stringify(data);
+        fetch("/login", {
+            method: "POST",
+            body: data
+        });
+    }; 
 });
+
